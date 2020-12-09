e61b8f80316c5ae6312b3967edb0b2874a290ee2
Gabriel Jahbez <45593692+gabrieljmw@users.noreply.github.com>
2020-12-09 16:07:55 -0500

    New JS implementation

--- js/script.js
+++ js/script.js
@@ -1,30 +1,37 @@
-let crashAudio = new Audio('audio/crash.wav');
-let highHatClosedAudio = new Audio('audio/highhat-closed.wav');
-let highHatOpenAudio = new Audio('audio/highhat-open.wav');
-let kickAudio = new Audio('audio/kick.wav');
-let snareAudio = new Audio('audio/snare.wav');
-let tom1Audio = new Audio('audio/tom1.wav');
-let tom2Audio = new Audio('audio/tom2.wav');
-let tom3Audio = new Audio('audio/tom3.wav');
+// new implementation (Using HTML data-attributes)
 
-let drumKitAudio = {
-    crash: crashAudio,
-    highHat: highHatOpenAudio,
-    kick: kickAudio,
-    snare: snareAudio,
-    tom1: tom1Audio,
-    tom2: tom2Audio,
-    tom3: tom3Audio
-};
+let drumKit = document.querySelectorAll('.playable');
 
-let drumKit = document.querySelectorAll('#drumKit g');
-for (let item of drumKit) {
-    for (let key = 0; key < Object.keys(drumKitAudio).length; ++key) {
-        if (item.id == Object.keys(drumKitAudio)[key]) {
-            // document.write(`${item.id} equals ${Object.keys(drumKitAudio)[key]} is true`)
-            document.querySelector(`#${Object.keys(drumKitAudio)[key]}`).addEventListener('click', (e) => {
-                Object.values(drumKitAudio)[key].play();
-            })
-        }
-    }
-}
+drumKit.forEach((drum) => {
+    let audioPath = drum.getAttribute('data-audio');
+    let createAudio = new Audio(audioPath);
+    drum.addEventListener('click', () => {
+        createAudio.play();
+    });
+});
+
+
+
+// Previous rendition (using Javascript primarily)
+
+// let drumKitAudio = {
+//     crash: new Audio('audio/crash.wav'),
+//     highHat: new Audio('audio/highhat-open.wav'),
+//     kick: new Audio('audio/kick.wav'),
+//     snare: new Audio('audio/snare.wav'),
+//     tom1: new Audio('audio/tom1.wav'),
+//     tom2: new Audio('audio/tom2.wav'),
+//     tom3: new Audio('audio/tom3.wav')
+// };
+
+// let drumKit = document.querySelectorAll('#drumKit g');
+
+// for (let item of drumKit) {
+//     for (let key = 0; key < Object.keys(drumKitAudio).length; ++key) {
+//         if (item.id == Object.keys(drumKitAudio)[key]) {
+//             document.querySelector(`#${item.id}`).addEventListener('click', () => {
+//                 Object.values(drumKitAudio)[key].play();
+//             })
+//         }
+//     }
+// }
